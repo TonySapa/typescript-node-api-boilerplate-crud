7b6b8b1bdd06593f8102c3e167321e79402d3711
Tony <49716479+TonySapa@users.noreply.github.com>
2024-01-12 22:50:07 +0100

    deleted unused coverage tool

--- build/src/domain/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USER_LANGUAGES = exports.typesOfAccountStatus = void 0;
-exports.typesOfAccountStatus = ['unconfirmed', 'active', 'suspended'];
-exports.USER_LANGUAGES = ['es-ES', 'en-US'];
